4c17f876c951bfcd8863caca6642a7c2c094a3b9
Abhirup Roy <109097472+AbhirupRoy27@users.noreply.github.com>
2025-01-13 01:09:01 +0530

    Create bubble_sort.java

---
+++ bubble_sort.java
@@ -0,0 +1,17 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 4, 2};
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
